ba84dcd41e27f5b80a91d049ebcaf812a22426cc
Rodrigo Pascual Montero <rodrigopascual1@hotmail.com>
2021-02-18 20:51:46 +0100

    [ADD] Nuevo partido empezado

---
+++ views/Partido.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="partido">
+        <form class="formulario">
+            <fieldset>
+                <legend>Nuevo partido</legend>
+                <select class="local">
+                    <option value="0">Equipo local</option>
+                </select>
+            </fieldset>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+    .formulario{
+        margin-left: 500px;
+        
+    }
+    .local{
+        float: left;
+    }
+</style>
